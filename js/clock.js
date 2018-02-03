077e5789a51429a214a8a40504c1b4e112d565b7
Ernesto Hernandez <h_ernesto@ymail.com>
2018-02-03 13:14:39 -0800

    clock shows 12

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,4 @@
+/** VanillaJS to grab the id of the clock element */
 const clock = document.getElementById('clock');
 
 function Clock() {
@@ -6,22 +7,28 @@ function Clock() {
   let minutes = time.getMinutes().toString();
   let seconds = time.getSeconds().toString();
 
+  /** If hours are single digit, add a zero before the hour */
   if (hours.length < 2) {
-    hours = '0' + hours;
+    if(hours === "0"){
+      hours = "12";
+    }
+    else {hours = '0' + hours;}
   }
-
+  /** If minutes are single digit, add a zero before the minute */
   if (minutes.length < 2) {
     minutes = '0' + minutes;
   }
-
+  /** If seconds are single digit, add a zero before the second */
   if (seconds.length < 2) {
     seconds = '0' + seconds;
   }
-
+  /** The string will concatenate all the time variables */
   let clockStr = hours + ' : ' + minutes + ' : ' + seconds;
-
+  /** The text of the clock element will display the time */
   clock.textContent = clockStr;
 }
 
+/** Perform the Clock function */
 Clock();
+/** Perform the Clock function every second */
 setInterval(Clock, 1000);
