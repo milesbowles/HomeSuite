a0cde722bdf44f0552886afd12853ccd855b7312
Aram <avalbaro@gmail.com>
2018-02-06 00:03:16 -0800

    I have updated build files, I got the react app to load. However I wasn't able to get my calculator to load. I have put my calc work in the component files.

--- HomeSuite/client/src/App.js
+++ HomeSuite/client/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const welcome = "Welcome to React";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Welcome />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -17,5 +21,15 @@ class App extends Component {
     );
   }
 }
+
+
+class Welcome extends Component {
+  render() {
+    return (
+      <h1 className="App-title">{welcome}</h1>
+    )
+  }
+}
+
 
 export default App;
