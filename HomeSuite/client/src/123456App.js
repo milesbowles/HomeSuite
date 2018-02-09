99badbfeb671245bf43d717401e37ed88213d1ce
Aram <avalbaro@gmail.com>
2018-02-08 16:01:50 -0800

    I have a working calc app

---
+++ HomeSuite/client/src/123456App.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react';
+
+// import ReactDOM from "react-dom";
+
+// import './App.css';
+
+
+
+
+
+// class App extends Component {
+//   render(calculator) {
+//     return (
+       
+//       <div id="wrapper">
+//         <div id="App"></div>
+//       </div>
+  
+//     );
+//   }
+// }
+
+
+
+
+// export default App;
