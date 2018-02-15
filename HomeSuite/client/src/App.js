28ba8667615a3bfbc0e148ec9d808a811b789fb2
Miles <Milesvbowles@gmail.com>
2018-02-15 14:14:22 -0800

    Fixed calculator component styling

--- HomeSuite/client/src/App.js
+++ HomeSuite/client/src/App.js
@@ -3,15 +3,15 @@ import React from "react";
 // import "./App.test.js";
 /** This concatenated import is possible because the index file in /pages exports all the files together */
 // import {MainPage, Up, Down, Left, Right, UpLeft, UpRight, DownLeft, DownRight} from "./pages";
-// import {All} from "./pages";
+import {All} from "./pages";
 import Stocks from "./component/stocks";
 /** Define the application usage */
 const App = () =>
       /** A wrapper that encapsulates everything */
       <div className="site-wrap">
         <div className="panel-wrap animate--none">
-          {/* <All/> */}
-          <Stocks/>
+          <All/>
+          {/* <Stocks/> */}
         </div>
       </div>
 export default App;
