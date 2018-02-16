33c6638a97303db625f9054623cf1f0b4e8fad37
Ernesto Hernandez <h_ernesto@ymail.com>
2018-02-15 19:25:40 -0800

    stock component created

--- HomeSuite/client/src/component/stocks/Stocks.js
+++ HomeSuite/client/src/component/stocks/Stocks.js
@@ -1,27 +1,50 @@
 import React, {Component} from "react";
 import API from "../../utils/stocksAPI";
+import Search from "./stockForm";
 
 class Stocks extends Component{
     state = {
         companies: ["AAPL", "FB", "SBUX", "NKE", "VZ"],
-        amounts: []
+        amounts: [],
+        search: ""
     };
     /** When the page loads */
     componentDidMount(){
-        /** Find stock info for each company above */
-        for (var i = 0; i < this.state.companies.length; i++){
-            this.getStockInfo(this.state.companies[i], this.state.amounts);
-        }
+        
     };
     /** This function calls on the API folder to perform a get request */
     /** It takes in a company to search as a parameter */
     getStockInfo(company){
-        API.getStocks(company);
+        var temp = this.state.amounts;
+        API.getStocks(company).then(data => {temp.push(data.data.dataset_data.data[0][4]); this.setState({amounts: temp})});
     };
+    handleInputChange = event => {
+        /** Refer to the name of the element */
+        const name = event.target.name;
+        /** Refer to the value of the target */
+        const value = event.target.value;
+        /** Change the state to the value provided by the user */
+        this.setState({
+            /** Name of the element gets the value */
+            /** Without the [] then name is undefined because name is not a state variable */
+            /** With the [] then if name is "search", the search variable gets the value */
+            [name]: value
+        });
+    };
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getStockInfo(this.state.search);
+    }
     render(){
         return (
-            
-            <h1>Hello</h1>
+            <div>
+            <Search
+                handleInputChange={this.handleInputChange}
+                value={this.state.search}
+                handleFormSubmit={this.handleFormSubmit}
+            />
+                <h1>{this.state.amounts.map(amount => {return amount})}</h1>
+            </div>
         );
     }
 };
---
+++ HomeSuite/client/src/component/stocks/stockForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+const Search = props =>
+    <form>
+        <div className="form-group row center-block">
+            <div className="col-md-3"></div>
+            <div className="col-md-1">
+                <label htmlFor="search">Search:</label>
+            </div>
+            <div className="col-md-4">
+                <input
+                    onChange={props.handleInputChange}
+                    value={props.value}
+                    name="search"
+                    type="text"
+                    className="form-control"
+                    placeholder="Search For a Stock"
+                    id="search"
+                />
+            </div>
+            <div className="col-md-1">
+                <button onClick={props.handleFormSubmit} className="btn btn-primary">
+                    Search
+                </button>
+            </div>
+        </div>
+    </form>;
+export default Search;
