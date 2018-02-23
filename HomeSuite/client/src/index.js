863ca448a6834aac0f08cd139197378e8c3a5f94
Ernesto Hernandez <h_ernesto@ymail.com>
2018-02-23 10:43:16 -0800

    changes for redux

--- HomeSuite/client/src/index.js
+++ HomeSuite/client/src/index.js
@@ -1,45 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-render((
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
-	), document.getElementById('root'));
-registerServiceWorker();
-
-// import "babel-polyfill";
-// import React from 'react'
-// import { render } from 'react-dom'
-// import { Provider } from 'react-redux'
-// import { createStore, applyMiddleware } from 'redux'
-// import thunkMiddleware from 'redux-thunk'
-// import { createLogger } from 'redux-logger'
-// import suiteApp from './reducers/reducers'
-// // import {fetchStocks} from "./actions/actions";
-// import App from './App'
-
-// const loggerMiddleware = createLogger()
-
-// const store = createStore(
-//   suiteApp,
-//   applyMiddleware(
-//     thunkMiddleware, // lets us dispatch() functions
-//     loggerMiddleware // neat middleware that logs actions
-//   )
-// )
-
-// // store
-// //   .dispatch(fetchStocks("AAPL"))
-// //   .then(() => console.log(store.getState()));
-
-// render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// )
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import Root from './containers/Root'
+ 
+render(
+  <Root />,
+  document.getElementById('root')
+)
